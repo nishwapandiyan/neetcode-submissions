36b3f59a737f9c35cf73a31b129106b337ebf659
R.P.Nishwa <nishwapandiyan@gmail.com>
2026-06-20 17:03:02 +0530

    Add: permutations - submission-1

---
+++ Data Structures & Algorithms/permutations/submission-1.py
@@ -0,0 +1,7 @@
+from itertools import permutations
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        return [list(p) for p in permutations(nums)]
+        # for p in permutations(nums):
+        #     return [list(p)]
+        
